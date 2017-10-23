ecdf5574cafd8e958e5d08a9107fa1b60ded040e
zaphenic <33022283+zaphenic@users.noreply.github.com>
2017-10-23 16:46:56 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HomePage</title>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" type="text/css" href="./jquery/jquery.mobile-1.4.5.css">
+	<script type="text/javascript" src="./jquery/jquery-1.10.2.min.js"></script>
+	<script type="text/javascript" src="./jquery/jquery.mobile-1.4.5.min.js"></script>
+	<script>
+		self.location="HomePage.php";
+	</script>
+</head>
+<body>
+
+	<div data-role="page">
+		<div data-role="header"></div>
+
+		<div role="main" class="ui-content"></div>
+
+		<div data-role="footer"></div>
+		
+	</div>
+
+</body>
+</html>
